41c776c11203716405dccdd04bc30929d245d8df
fauzihub13 <fauziadisaputra3@gmail.com>
2024-11-03 19:09:46 +0700

    fix: sizing back button and camera option

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $('.btn-back').on('click', function() {
         window.location = "/";
     });
+    $('.btn-back-2').on('click', function() {
+        window.location = "/";
+    });
 });
